6665bbe56c9aaa138aa89b838666fd87becacfa2
azimeulker <azimeulker@hotmail.com>
2023-08-24 09:57:10 -0500

    finished the lockpicking prototype with rotation system

--- MiniGameJam/Assets/Scripts/DragAndDrop1.cs
+++ MiniGameJam/Assets/Scripts/DragAndDrop1.cs
@@ -9,12 +9,31 @@ public class DragAndDrop : MonoBehaviour
     public DragEndedDelagate dragEndedCallback;
 
     private bool isDragged = false;
+    private bool isSelected;
     private Vector3 mouseDragStartPosition;
     private Vector3 spriteDragStartPosition;
+
+    //rotation
+    private Quaternion initialRotation;
+    private float rotationSpeed = 10f;
+
+    private void Update()
+    {
+        //Rotate the selected object using the scroll wheel
+        if (isSelected)
+        {
+            float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+            if(scrollWheel != 0)
+            {
+                transform.Rotate(Vector3.forward, scrollWheel * rotationSpeed);
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
         isDragged = true;
+        isSelected = true;
         mouseDragStartPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         spriteDragStartPosition = transform.localPosition;
     }
@@ -30,6 +49,7 @@ public class DragAndDrop : MonoBehaviour
     private void OnMouseUp()
     {
         isDragged = false;
+        isSelected = false;
         dragEndedCallback(this);
     }
 }
